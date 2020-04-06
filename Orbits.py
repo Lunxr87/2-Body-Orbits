e91babbff313dc248d920079f909917d699efeee
Jeffrey Vickroy <62685752+Lunxr87@users.noreply.github.com>
2020-04-06 13:21:06 -0700

    Update Orbits.py

--- Orbits.py
+++ Orbits.py
@@ -4,11 +4,19 @@ Created on Thu Mar 12 22:53:19 2020
 
 @author: Lunxr
 """
-##### This code produces an accurate simulation of a 2-body problem #####
+##### This code produces a simulation of a 2-body problem #####
 
 #
 ###
 #
+
+# add comets
+# add stellar type (color)
+# add light and dark side for planet
+# add roche limit check and orbit spiral
+# add plots of position vs time
+# forces -> a = -GM/r^2 
+
 #
 ###
 #
@@ -16,16 +24,13 @@ Created on Thu Mar 12 22:53:19 2020
 ### Libraries ###
 import math as mt # need math functions
 from math import pi # need pi value
+import matplotlib.pyplot as plt
 import turtle # use to draw
-from random import seed # not actually sure what this is used for
+from random import seed # used for same sequence of random values 
+                        # best used for debugging
 from random import randint # random integer values
 
-#
-###
-#
-#
-###
-# 
+
 
 ### Star or Planet ###
 # this function uses the input of planet or star to change the wording of my code based on answer
@@ -46,15 +51,9 @@ else:
     unit = "Solar"
     phrase = " companion "
 orbits = int(input("How many orbits would you like to see (1,2,3,...)? * ")) # run code for this long
-## Works ##
 
-#
-###
-#
-#
-###
-# 
- 
+
+
 ### Scaling ###
 # this function and part of code is used to scale up the distances per pixel
 def real(ans): 
@@ -65,39 +64,34 @@ def real(ans):
 # ask the user if they want realism or not, the boole statements will scale accodingly
 pregunta = str(input("Would you like a realistic system? (Y/N) * "))
 respuesta = real(pregunta) # calls function to be checked, which then uses appropriate scale factors
-if respuesta == True: # will ask again since this is not ideal
-    pregunta = str(input("Are you sure? You won't see much if yes. (Y/N) * "))
-    respuesta = real(pregunta)
-    if respuesta == True: 
+if respuesta == True: # will ask again since realism is not ideal
+    stopper = str(input("Are you sure? You won't see much if yes. (Y/N) * "))
+    again = real(stopper)
+    if again == True: 
         d = 1
         n = 1
     else:
-        if answer == "planet": # want to fit system in the window to demonstrate whats going on
-            d = 10
-            n = 3
-        if answer == "star": # the distances will inherently be large so scale down a lot
-            d = 20
-            n = 0.5
+        no_real = 'no'
+        respuesta = real(no_real)
 else:
-    if answer == "planet": # want to fit system in the window to demonstrate whats going on
-        d = 10
-        n = 3
-    if answer == "star": # the distances will inherently be large so scale down a lot
-        d = 20
+    if answer == "planet": 
+        n = 2
+        d = 350
+    if answer == "star": # used to scale object size
         n = 0.5
-## Works ##
+        d = 350
 
-#
-###
-#
-#
-###
-# 
+
 
 ### Variables ###
-# user inputs
+## user inputs ##
 print("\nWhen the simmulation is done, click on one of the stars to close out.")
-a = float(input(f"What is the semi-major axis of your {answer:s} in AU?\nFor reference, 1 AU is the distance between the Sun and Earth.\nSaturn is ~10 AU. * "))
+print("Semi-major axis is the distance between your object and the center of mass.")
+if answer == "star":
+    print("Note that a binary star system might have a value > 1000 AU.")
+elif answer == "planet":
+    print("For reference, Jupiter is ~5 AU while Saturn is ~10 AU away from the Sun.")
+a = float(input(f"What is the semi-major axis of your {answer:s} in AU?\n1 AU is the distance between the Sun and Earth. * "))
 ecc = float(input("What is the eccentricity of your orbit (a value between 0.0-1.0)? * "))
 print("\nYou might want to use numbers > 0.0 and < 10.0 but any number works.")
 if answer == "star":
@@ -106,27 +100,57 @@ star_r = float(input("In Solar Radii, how large is your primary star? * "))
 star_m = float(input("In Solar Masses, how massive is your primary star? * "))
 object_r = float(input(f"In {unit:s} Radii, how large is your{phrase:s}{answer:s}? * "))
 object_m = float(input(f"In {unit:s} Masses, how massive is your{phrase:s}{answer:s}? * "))
-# initialized variables
+
+## initialized variables ##
 phi = 0
-i = 0
+t = 0
 x = y = 0 
 years = 0
-# Variable Scaling for distance
-if a >= 40:
-    d = 30
-elif a <= 10:
-    d == 0.25
-## Works ##
 
-#
-###
-#
-#
-###
-# 
+
+"""
+# needs fixing and finetunning 
+## variable scale factors ##
+# looking at scales with 'a' more likely for stars
+if a > 100 and a <= 500:
+    d == 300
+elif a > 500 and a <= 1000:
+    d == 250
+elif a > 1000 and a <= 1500:
+    d == 200 
+elif a > 1500 and a <= 2000:
+    d == 150
+elif a > 2000:
+    d == 100 
+# looking at scales with 'a' more likely for planets
+elif a > 40 and a <=100:
+    d == d
+elif a > 10 and a <= 40:
+    d == 300
+elif a <= 10:
+    d == 250     
+"""
+
 
 ### Conversions ###
-# conversions manipulated by n and d respecively for scaling (see 'Scaling' above)
+"""
+# gravitational constant
+G = 6.674*10**(-11) # m^3 kg^-1 s^-2
+m_to_au = (1.496*10**11)**3 # conversion for m^3 to au^3
+kg_to_sm = 1.989*10**30 # conversion for kg to solar mass
+new_G = G*kg_to_sm / m_to_au # G in au, solar mass, and seconds
+"""
+# convert AU to solar radii for a
+new_a = (a / 0.00465) # 1 solar radii = 0.00465 AU
+
+# these are more initial conditions for two bodies but are solely used for making objects
+r_o_top = (new_a*(1 - ecc**2))
+scale = r_o_top / d
+in_r_o = ((new_a*(1 - ecc**2)) / (1 + (ecc*mt.cos(0)))) / scale
+# radius at phi = 0
+focal = (new_a*ecc) / scale # focal point of orbit is where star will be
+
+# conversions manipulated by n for scaling (see 'Scaling' above)
 if answer == "planet":
     # need to convert planet radius to solar radii
     new_object_r = n*(object_r / 9.951) # 1 solar radii = 9.951 jupiter radii
@@ -138,35 +162,32 @@ if answer == "star": # units will be the same for both so only need pixel scale
     new_object_r = n*object_r
     new_star_r = n*star_r
     new_object_m = object_m
-# now the two radii are in the same units but not same units as semi-major axis (a)
-new_a = (a / 0.00465) / d # 1 solar radii = 0.00465 AU
-# want to show years on object in Earth years -> a must be in AU
-p = ((a**3) / (star_m + new_object_m))**(1/2) # period of blue object   
-## Works ##
+# now the radii and a are all in solar radii, and masses are in solar masses
 
-#
-###
-#
-#
-###
-# 
+# want to show years on object in Earth years -> a must be in AU
+p = ((a**3) / (star_m + new_object_m))**(1/2) # period of blue object 
+period = p*31540000 # 1 year = 31540000 sec -> Orbit in Earth years
+
+vel_o_sum = (2*pi*in_r_o*695700*scale) / period # initial orbital velocity 
+
+# lists used for plot
+plist = [0]
+vlist = [vel_o_sum]
+
+
 
 ### Window settings ###
 # window setup
 wn = turtle.Screen()
 wn.title("Orbits -- 2 Body Problem")
 wn.bgcolor("black")
-wn.setup(width=1350, height=850) # may vary depending on computer used
-## Works ##
+wn.setup(width=1350, height=850) # may vary depending on computer screen
+wn.setworldcoordinates(-675, -425, 675, 425) # sets coordinates of screen
 
-#
-###
-#
-#
-###
-# 
 
-### Star background ###
+
+### Draws Elements ###   
+## Star background ##
 # places a point representing a star at a random place in the window
 bg = turtle.Turtle()
 bg.hideturtle()
@@ -174,11 +195,11 @@ bg.color("white")
 bg.shape("circle")
 bg.penup()
 bg.speed(0) # no animations -> maximizes speed
-seed(1) # seed random number generator
+seed(87) # seed random number generator
 for n in range(200): 
     # vary the size of the shapes with slight differences
-    val = randint(1,5)
-    new_val = val / 15
+    val = randint(1,10)
+    new_val = val / 40
     bg.shapesize(new_val, new_val) # want small circles
     # generates random x and y coordinates
     valx = randint(-675,675)
@@ -187,24 +208,12 @@ for n in range(200):
     bg.goto(valx,valy) # sends to random coordinates
     bg.stamp() # stamps a white circle at the current coordinate
     wn.update() # essentially cements the stamps into the screen
-## Works ##
 
-#
-###
-#
-#
-###
-# 
-
-### Draws Elements ###     
 ## Bodies ##
-# these are more initial conditions for two bodies but are solely used for making objects
-in_r_o = (new_a*(1 - ecc**2)) / (1 + ecc) # radius at phi = 0
-focal = new_a*ecc # focal point of orbit is where star will be
 # Creates Star
 star = turtle.Turtle()
 star.penup()
-star.goto(-focal,0) # the star will be always be opposite side of CoM than object
+star.setpos(-focal,0) # the star will be always be opposite side of CoM than object
 star.shape("circle")
 star.color("yellow")
 star.shapesize(new_star_r, new_star_r) # star and object arent same size
@@ -212,7 +221,7 @@ wn.update() # update for same reason as star background
 # Creates Object
 obj = turtle.Turtle()
 obj.penup()
-obj.goto(in_r_o,0) # puts objectt at starting point on +x axis
+obj.setpos(in_r_o,0) # puts objectt at starting point on +x axis
 obj.shape("circle")
 obj.color("blue")
 obj.shapesize(new_object_r, new_object_r) # object and star arent the same size
@@ -231,22 +240,18 @@ path_o.hideturtle()
 path_o.penup()
 path_o.color("grey")
 path_o.pensize(0.2) # want small trail
-
-## Origin/ CoM ##
+"""
+## Origin as CoM ##
 org = turtle.Turtle()
 org.hideturtle()
 org.color("red")
-org.pensize(5)
+org.pensize(10)
 org.speed(0)
 org.penup()
-org.goto(-10,10)
-org.pendown()
-org.goto(10,-10)
+org.setpos(0,0) # CoM is at the origin
+org.write('Center of Mass', align='center')
 org.penup()
-org.goto(10,10)
-org.pendown()
-org.goto(-10,-10)
-
+"""
 ## Creates Legend scale for the unrealistic scale ##
 scale_dist = in_r_o
 if respuesta == False:
@@ -256,23 +261,23 @@ if respuesta == False:
     leg.speed(0) # want this made as quickly as possible
     # makes horizontal component the length of r_p
     leg.penup()
-    leg.goto(600,-355)
+    leg.goto(600,-370)
     leg.pendown()
-    leg.goto(600 - scale_dist,-355)
+    leg.goto(600 - scale_dist,-370)
     # goes back to right side to make vertical part
     leg.penup()
-    leg.goto(600,-355)
+    leg.goto(600,-370)
     leg.pendown()
-    leg.goto(600,-345)
+    leg.goto(600,-360)
     # goes to left side to make other vertical part
     leg.penup()
-    leg.goto(600 - scale_dist,-355)
+    leg.goto(600 - scale_dist,-370)
     leg.pendown()
-    leg.goto(600 - scale_dist,-345)
+    leg.goto(600 - scale_dist,-360)
     # prints how large the scale is in AU reference is distance at closest approach
     leg.penup()
-    leg.goto(600,-340)
-    leg.write(f"{a:,.1f} AU", align="right")
+    leg.goto(600,-355)
+    leg.write(f"{a:,.3f} AU", align="right")
     
 ## Creates Legend for properties ##
 prop = turtle.Turtle()
@@ -319,29 +324,31 @@ pen_t.goto(600,-310)
 # makes Earth Year Text
 prop.penup()
 prop.goto(330,-340)
-prop.write(f"1 year on blue {answer:s} = {p:,.1f} Earth years", align="left")
-## Works ##
+prop.write(f"1 year on blue {answer:s} = {p:,.3f} Earth years", align="left")
 
-#
-###
-#
-#
-###
-# 
+
 
 ### Circular Motion ###
 # Main loop that makes the star and planet move relative to CoM
 while True:
-    while phi <= orbits*2*pi: # want multiple orbits
-        if i <= (200*orbits + 1): # want small increments for more accurate simulation 
-            phi = (i*pi) / 100 # small theta values for same increment reason
+    while phi <= 2*pi*orbits: # want multiple orbits
+        if t <= (200*orbits + 1): # want small increments for more smooth motion
             
-            # planet time #
-            r_o = (new_a*(1 - ecc**2)) / (1 + (ecc*mt.cos(phi))) # correct radial equation from CoM
-            x_o = r_o*mt.cos(phi) + new_a*ecc 
+            ## EoM ##
+            phi = (t*pi) / 100 # small theta values for same increment reason
+            
+            # object time #
+            r_o = ((new_a*(1 - ecc**2)) / (1 + (ecc*mt.cos(phi)))) / scale # radial equation from object to CoM
+            """
+            F = - new_G*star_m*new_object_m / (r_o**2)
+            a_o = F / new_object_m
+            ax_o = a_o*mt.cos(phi)
+            ay_o = a_o*mt.sin(phi)
+            """
+            x_o = r_o*mt.cos(phi) + ((new_a*ecc) / scale) 
             y_o = r_o*mt.sin(phi)
-            obj.goto(x_o,y_o) # send planet to updated position
-            # makes path that follows planet
+            obj.goto(x_o,y_o) # send object to updated position
+            # makes path that follows object
             path_o.goto(x_o,y_o)
             path_o.pendown()
     
@@ -354,34 +361,53 @@ while True:
             path_s.goto(x_s,y_s)
             path_s.pendown()
             
+            ## Features ##
+            
             # for legend #
             # convert to AU and account for scaling. all the values in r_diff eq are in solar radii
             # used to show difference between distance of objects
-            r_diff = ((r_s*d) - (r_o*d) + (new_object_r / n) + (new_star_r / n))*0.00465
-            mag_r_diff = abs(r_diff)
-            period = p*31540000 # 1 year = 31540000 sec
-            v_p = (2*pi*r_o*695700*d) / period # 1 solar radii = 695700 km. account for scaling
+            
+            # not right
+            r_diff = ((r_s*scale) - (r_o*scale) + (new_object_r / n) + (new_star_r / n))*0.00465 # turns to AU
+            mag_r_diff = abs(r_diff) # just magnitude
+            
+            
+            v_o = (2*pi*r_o*695700*scale) / period # 1 solar radii = 695700 km. account for scaling
             # want to print current position velocity and number of orbits
             pen_pos.clear()
-            pen_pos.write(f"{mag_r_diff:,.2f} AU", align="right")
+            pen_pos.write(f"{mag_r_diff:,.3f} AU", align="right")
             pen_vel.clear()
-            pen_vel.write(f"{v_p:,.2f} km/s", align="right")
+            pen_vel.write(f"{v_o:,.3f} km/s", align="right")
             pen_t.clear()
-            pen_t.write(f"{years:,.2f}", align="right")
+            pen_t.write(f"{years:,.3f}", align="right")
             
+            # plot lists #
+            vlist = vlist + [v_o]
+            plist = plist + [years]
+            
+            ## Iterables ##
+            vel_o_sum += v_o
             years += 0.005
-            i += 1
-            if i == (200*orbits + 1): # time to stop
-                phi = 10000
-                turtle.exitonclick() # when the window is clicked the program ends
-            wn.update() # want to update window to prevent duplicates
-## Works ##
-
-#
-###
-#
-#
-###
-# 
+            t += 1
             
-#### observe this system ####
+            if t == (200*orbits + 1): # time to stop
+                phi = 1002
+                ave_vel_o = vel_o_sum / (200*orbits + 1)
+                print(f'Average Orbital Velocity = {ave_vel_o:,.3f} km/s')
+                
+                ## plots ##
+                pmin = float(min(plist))
+                pmax = float(max(plist))
+                vmin = float(min(vlist)) - 0.5
+                vmax = float(max(vlist)) + 0.5
+                plt.title('Orbital Velocity Evolution')
+                plt.xlabel(f"Time (years on {answer:s})")
+                plt.ylabel(f"Velocity of {answer:s} (km/s)")
+                plt.axis([pmin,pmax,vmin,vmax])
+                plt.plot(plist, vlist, 'o', markersize = 4, color = 'blue', linestyle = '--', linewidth = 1.0)
+                plt.show()
+                
+                turtle.exitonclick() # when the window is clicked the program ends
+                
+            wn.update() # want to update window to show object move without leaving its impression            
+#### observe this planetary system ####
